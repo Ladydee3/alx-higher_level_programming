0f429a54d2d6b30c4c45772aaaa7a2dcccec632d
Ladydee3 <delisiletwala62@gmail.com>
2023-11-30 11:38:00 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
-#!/bin/python3
-if_name_ ==" _main+":
+#!/usr/bin/python3
+if __name__ == "__main__":
     import hidden_4
-    #Print sorted name from directory
+    # Print sorted name from directory
     for name in sorted(dir(hidden_4)):
-        #print only names that do not start with_
-        if name[:2] != '_':
+        # print only names that do not start with __
+        if name[:2] != '__':
             print("{}".format(name))
