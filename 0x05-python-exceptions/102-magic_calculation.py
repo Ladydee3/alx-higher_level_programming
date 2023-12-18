521494a280e70e3c3f17a54c316ab5dac7c64d61
Ladydee3 <delisiletwala62@gmail>
2023-12-18 11:58:43 -0800

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+
+
+
 def magic_calculation(a, b):
-    result = 0
-    for num in range(1, 3):
-        try:
-            if num > a:
-                raise Exception:
-                    result = b + a
-                    break
-                return (result)
+    """A function based on a python bytee code"""
+    return (result)
