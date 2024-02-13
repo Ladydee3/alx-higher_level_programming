247237a7aef46d9c177bca8612d219f54d31d968
Ladydee3 <delisiletwala62@gmail.com>
2024-02-13 06:17:55 -0800

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
---list all rows of the table second_table
+-- list all rows of the table second_table
 -- from database hbtn_0c_0
 -- result display score and name
 -- ordered by score
-with score >= 10
+-- with score >= 10
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
