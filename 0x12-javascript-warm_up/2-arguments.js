839bb6f6feb1783b7b28577ea36ea14b78ea6e02
Ladydee3 <delisiletwala62@gmail.com>
2024-03-11 09:10:07 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.arg.legth == 2) {
+	console.log('No argument');
+} else if (process.arg.length == 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments foumd');
+}
