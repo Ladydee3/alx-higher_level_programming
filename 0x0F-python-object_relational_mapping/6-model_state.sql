2881c8d97a3ba78dd95e07ded55e28f6e419090e
Ladydee3 <delisiletwala62@gmail.com>
2024-03-16 10:47:24 -0700

    6-model_state.py

--- 0x0F-python-object_relational_mapping/6-model_state.sql
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -1,8 +1,4 @@
 -- Create database hbtn_0e_6_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 USE hbtn_0e_6_usa;
-SHOW CREATE TABLE states;
-
-Enter password: 
-Table   Create Table
-states  CREATE TABLE `states` (\n  `id` int(11) NOT NULL AUTO_INCREMENT,\n  `name` varchar(128) NOT NULL,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=latin1
+SHOW CREATE TABLE state;
