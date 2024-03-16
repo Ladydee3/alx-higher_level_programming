742bfe2f7882afadd280b8a21fa1915ac83569e4
Ladydee3 <delisiletwala62@gmail.com>
2024-03-16 12:41:26 -0700

    changes

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
+""" prints the first State object from the database hbtn_0e_6_usa
 """
-List all State objects with a
-"""
-if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-    mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
-                                                             sys.argv[2],
-                                                             sys.argv[3])
-    engine = create_engine(mysql)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for item in session.query(State).filter(State.name.contains('a')):
-        print("{:d}: {:s}".format(item.id, item.name))
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
