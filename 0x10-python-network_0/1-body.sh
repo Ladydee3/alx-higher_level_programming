48b1cc064b8ecec42bcbfd921f34166fbed5c17e
Ladydee3 <delisiletwala62@gmail.com>
2024-03-27 10:17:38 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#shellcheck disable=SC2046
-if [$(curl -l -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -ls "$1";
+# shellcheck disable=SC2046
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
