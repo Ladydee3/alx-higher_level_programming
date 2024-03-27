170b25f40ba3725990c388ab9a717918bbdb6d32
Ladydee3 <delisiletwala62@gmail.com>
2024-03-27 10:07:34 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin//bash
-#script that gets body size of request
-curl -ls "$" | grep -w 'Content-Length' | cut -f2 -d' '
+#!/bin/bash
+# script to get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
