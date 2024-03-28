4c29c6eb8a6a42202538e2c54ae2db4a8b09b217
Ladydee3 <delisiletwala62@gmail.com>
2024-03-28 13:55:36 -0700

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+take in a URL, sends a request to the URL and displays
+the body of the response (decode in utf-8).
+"""
+
+import sys
+from urllib import request, error
+
+if __name__ == "__main__":
+    url = sys.arg[1]
+
+    own_request = request.Request(url)
+    try:
+        with request.urlopen(own_request) as respond:
+            print(response.read().decode("ascii"))
+    except error.HTTPError as e:
+        print("Error code: {}"format(e.code))
