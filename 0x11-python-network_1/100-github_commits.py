7c409f72a796293cda2fa937dc90c2b032b3d0c1
Ladydee3 <delisiletwala62@gmail.com>
2024-03-28 17:22:16 -0700

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,46 +1,43 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-
-Usage: ./100-github_commits.py <repository name> <repository owner>
-
-FUNCTIONALITY
-
-1. First, we import the required packages,
-    requests and sys.
-
-2. We get the repository name and owner name from the
-    command line arguments using the sys module.
-
-3. We set up the API endpoint URL using the format method to
-    insert the repo and owner names into the URL string.
-
-4. We make the API call using the requests.get method
-    and passing the URL as a parameter.
-
-5. We check the response status code to ensure that
-    the API call was successful.
-
-6. If the API call was successful, we iterate over the first 10 commits in the
-    response JSON and display the commit hash, author name, and date.
-
-7. If the API call was not successful,
-    we display the error status code.
-
 """
-import sys
+Module to access to the GitHub API and uses the information
+"""
 import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+def main(argv):
+    """
+    Function that list 10 commits (from the most recent to oldest)
+    of the repository.The first argument will be the repository name
+    and the second argument will be the owner name
+    """
+
+    def print_commits(i, commit_list):
+        """
+        List the commits, less than 10 commits
+        """
+        sha = commit_list[i].get('sha')
+        commit = commit_list[i].get('commit')
+        author = commit.get('author')
+        name = author.get('name')
+        print('{}: {}'.format(sha, name))
+
+    repo = argv[1]
+    owner = argv[2]
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    response = requests.get('https://api.github.com/repos/' + owner +
+                            '/' + repo + '/commits', headers=headers)
+    commit_list = response.json()
+    size = len(commit_list)
+    if size < 10:
+        for i in range(0, size):
+            print_commits(i, commit_list)
+    else:
+        for i in range(0, 10):
+            print_commits(i, commit_list)
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
-
-    response = requests.get(url)
-    commits = response.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    main(argv)
