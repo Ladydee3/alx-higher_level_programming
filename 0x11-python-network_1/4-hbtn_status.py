ca1f09270a9b8b3411c1540eb4ecef8aad2f984c
Ladydee3 <delisiletwala62@gmail.com>
2024-03-28 17:46:30 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-fetches https://alx-intranet.htbn.io/status
+fetches https://alx-intranet.hbtn.io/status
 """
 
 import requests
 
 if __name__ == '__main__':
-    r = requests.get('https://alx-intranet.htbn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
